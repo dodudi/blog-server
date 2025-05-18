3b9175e290dff33bdef4fb9f9b3be2f90e46a870
Kwon Deog Young <idejrdud@gmail.com>
2025-05-18 23:48:15 +0900

    fix: 스키마 수정

--- src/main/resources/sql/prod/schema.sql
+++ src/main/resources/sql/prod/schema.sql
@@ -1,9 +1,10 @@
 CREATE TABLE IF NOT EXISTS post
 (
     id               SERIAL PRIMARY KEY,
-    title            VARCHAR(255),
-    content          TEXT,
-    author           VARCHAR(255),
-    create_date_time TIMESTAMP(6),
-    update_date_time TIMESTAMP(6)
+    title            VARCHAR(255) not null,
+    content          TEXT not null,
+    author           VARCHAR(255) not null,
+    format           VARCHAR(255) not null,
+    create_date_time TIMESTAMP(6) not null,
+    update_date_time TIMESTAMP(6) not null
 );
