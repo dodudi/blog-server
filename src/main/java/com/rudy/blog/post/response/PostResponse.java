13a2e502ea54c619e97da42479f48f5aa1fcbb2f
Kwon Deog Young <idejrdud@gmail.com>
2025-05-18 22:06:53 +0900

    fix: LocaldateTime -> Instant 변경

--- src/main/java/com/rudy/blog/post/response/PostResponse.java
+++ src/main/java/com/rudy/blog/post/response/PostResponse.java
@@ -2,11 +2,8 @@ package com.rudy.blog.post.response;
 
 import com.rudy.blog.post.domain.Post;
 import lombok.Getter;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.time.Instant;
-import java.util.ArrayList;
-import java.util.List;
 
 @Getter
 public class PostResponse {
@@ -14,8 +11,6 @@ public class PostResponse {
     private final String title;
     private final String content;
     private final String author;
-    private final List<MultipartFile> images = new ArrayList<>();
-    private final List<MultipartFile> files = new ArrayList<>();
     private final Instant createDateTime;
     private final Instant updateDateTime;
 
@@ -24,15 +19,7 @@ public class PostResponse {
         this.title = post.getTitle();
         this.content = post.getContent();
         this.author = post.getAuthor();
-        this.createDateTime = Instant.from(post.getCreateDateTime());
-        this.updateDateTime = Instant.from(post.getUpdateDateTime());
-    }
-
-    public void addImage(MultipartFile image) {
-        this.images.add(image);
-    }
-
-    public void addFile(MultipartFile file) {
-        this.files.add(file);
+        this.createDateTime = post.getCreateDateTime();
+        this.updateDateTime = post.getUpdateDateTime();
     }
 }
